3e17a0e4815cd865239d9117017549cfb2076693
YujinJuhn <holsya@naver.com>
2024-05-27 23:46:54 +0900

    revert1

--- lib/nearby/widgets/panel_widget.dart
+++ lib/nearby/widgets/panel_widget.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'restaurant_info_widget.dart';
-import 'package:mealmatch/models/restaurant.dart';
+import 'package:mealmatch/services/data_manager.dart';
 
 class PanelWidget extends StatelessWidget {
   final ScrollController controller;
   final PanelController panelController;
-  final List<Restaurant> restaurants;
 
   const PanelWidget({
     Key? key,
     required this.controller,
     required this.panelController,
-    required this.restaurants,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final dataManager = Provider.of<DataManager>(context);
+
     return Column(
       children: <Widget>[
         buildDragHandle(),
@@ -34,7 +35,7 @@ class PanelWidget extends StatelessWidget {
                   ),
                 ),
               ),
-              buildRestaurantList(),
+              buildRestaurantList(dataManager),
               const SizedBox(height: 24),
             ],
           ),
@@ -60,20 +61,24 @@ class PanelWidget extends StatelessWidget {
 
   void togglePanel() => panelController.isPanelOpen ? panelController.close() : panelController.open();
 
-  Widget buildRestaurantList() {
-    int itemCount = restaurants.length < 20 ? restaurants.length : 20;
+  Widget buildRestaurantList(DataManager dataManager) {
+    int itemCount = dataManager.restaurants.length < 20 ? dataManager.restaurants.length : 20;
 
     return Column(
       children: List.generate(itemCount, (index) {
-        final restaurant = restaurants[index];
-        return RestaurantInfoWidget(
-          name: restaurant.enName,
-          category: restaurant.enCategory,
-          address: restaurant.enAddress,
-          openingHours: restaurant.enOpeningHours,
-          mainImages: restaurant.mainImages,
-          reviews: restaurant.reviews,
-        );
+        final restaurant = dataManager.restaurants[index];
+        if (restaurant != null) {
+          return RestaurantInfoWidget(
+            name: restaurant.enName,
+            category: restaurant.enCategory,
+            address: restaurant.enAddress,
+            openingHours: restaurant.enOpeningHours,
+            mainImages: restaurant.mainImages,
+            reviews: restaurant.reviews,
+          );
+        } else {
+          return SizedBox.shrink();
+        }
       }),
     );
   }
